d31ce58d29d8cdb3ffe38758dc1e15f832f0d5db
Daniella Jaeger <daniella@kickstarter.com>
2012-09-30 16:45:55 -0400

    put s3 detes back into model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ActiveRecord::Base
   attr_accessible :title, :photo, :url
   has_attached_file :photo, :styles => { :medium => "x300" },
     :storage => :s3,
-    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+    :s3_credentials => {
+          :bucket            => ENV['adventuresinshipping'],
+          :access_key_id     => ENV['AKIAIY4CKFUEEX555J6Q'],
+          :secret_access_key => ENV['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']
+        },
     :path => "/:style/:filename"
 end
