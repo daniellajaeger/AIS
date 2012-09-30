b4f42924b1ce8d5e79df988536f5d9828dd8d0fd
Daniella Jaeger <daniella@kickstarter.com>
2012-09-30 17:17:36 -0400

    config/s3.yml again

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,6 @@ class Post < ActiveRecord::Base
   attr_accessible :title, :photo, :url
   has_attached_file :photo, :styles => { :medium => "x300" },
     :storage => :s3,
-    :s3_credentials => {
-          :bucket            => ENV['adventuresinshipping'],
-          :access_key_id     => ENV['AKIAIY4CKFUEEX555J6Q'],
-          :secret_access_key => ENV['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']
-        },
+    :s3_credentials => YAML.load_file("#{Rails.root}/config/s3.yml"),
     :path => "/:style/:filename"
 end
