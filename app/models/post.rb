ce39535d6677dee3bc7317dcf0361a160568e3ae
Daniella Jaeger <daniella@kickstarter.com>
2012-09-30 17:21:46 -0400

    tweak credentials

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   attr_accessible :title, :photo, :url
   has_attached_file :photo, :styles => { :medium => "x300" },
     :storage => :s3,
-    :s3_credentials => YAML.load_file("#{Rails.root}/config/s3.yml"),
+    :s3_credentials => "#{Rails.root}/config/s3.yml",
     :path => "/:style/:filename"
 end
